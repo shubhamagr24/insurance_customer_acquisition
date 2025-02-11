bed66d91190bf95db7d73efc75d5df961da68136
shubhamagr24 <shubhamagr24@gmil.com>
2025-02-12 00:37:13 +0530

    data upload to mongo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="shubham agarwal",
+    author_email="shubhamagr24@gmail.com",
+    packages=find_packages()
+)
