961fa7c163a67c40505dbb45aecfeb3976b7ed13
shubhamagr24 <shubhamagr24@gmil.com>
2025-02-14 02:28:13 +0530

    test ran the data ingestion part of training pipeline

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -3,11 +3,11 @@ from src.exception import MyException
 from src.logger import logging
 
 from src.components.data_ingestion import DataIngestion
-from src.components.data_validation import DataValidation
-from src.components.data_transformation import DataTransformation
-from src.components.model_trainer import ModelTrainer
-from src.components.model_evaluation import ModelEvaluation
-from src.components.model_pusher import ModelPusher
+# from src.components.data_validation import DataValidation
+# from src.components.data_transformation import DataTransformation
+# from src.components.model_trainer import ModelTrainer
+# from src.components.model_evaluation import ModelEvaluation
+# from src.components.model_pusher import ModelPusher
 
 from src.entity.config_entity import (DataIngestionConfig,
                                         #   DataValidationConfig,
